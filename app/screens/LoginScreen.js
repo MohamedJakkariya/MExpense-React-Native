d39b30ebfe17ac754162c848b0934f5fc8880bb4
MohamedJakkariya <jacksparrow.mdjack@gmail.com>
2021-03-13 12:14:47 +0530

    added login screen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,5 +1,6 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { StyleSheet, View, TouchableOpacity, Text, Keyboard } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 import color from '../constants/color';
 import Logo from '../../assets/Logo.svg';
 import { TextInput } from 'react-native-paper';
@@ -8,6 +9,7 @@ import { TextInput } from 'react-native-paper';
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isSelected, setSelection] = useState(false);
 
   return (
     <View style={styles.container}>
@@ -18,45 +20,86 @@ const LoginScreen = () => {
       <View
         style={{
           width: '100%',
-          bottom: 200
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          bottom: 45
         }}
       >
-        <TextInput
-          selectionColor={color.white}
-          mode='outlined'
+        {/* Input box container  */}
+        <View
           style={{
-            width: '70%',
-            right: 20,
-            justifyContent: 'center',
-            alignSelf: 'flex-end',
-            backgroundColor: color.green,
-            color: color.white
+            width: '100%'
           }}
-          label='Email'
-          value={email}
-          dense={false}
-          onChangeText={text => setEmail(text)}
-        />
+        >
+          <TextInput
+            selectionColor={color.white}
+            mode='outlined'
+            style={{
+              width: '90%',
+              alignSelf: 'center',
+              backgroundColor: color.green,
+              fontSize: 18
+            }}
+            label='Email'
+            value={email}
+            dense={false}
+            onChangeText={text => setEmail(text)}
+          />
 
-        <TextInput
-          selectionColor={color.white}
-          mode='outlined'
+          <TextInput
+            selectionColor={color.white}
+            mode='outlined'
+            secureTextEntry={true}
+            autoCorrect={false}
+            style={{
+              width: '90%',
+              right: 20,
+              alignSelf: 'flex-end',
+              backgroundColor: color.green,
+              color: color.white,
+              fontSize: 18,
+              paddingTop: 10
+            }}
+            label='Password'
+            value={password}
+            onChangeText={text => setPassword(text)}
+          />
+        </View>
+
+        {/* Input box bottom  */}
+        <View
           style={{
-            width: '70%',
-            right: 20,
-            alignSelf: 'flex-end',
-            backgroundColor: color.green,
-            color: color.white
+            width: '90%',
+            flexDirection: 'row',
+            display: 'flex',
+            alignItems: 'center',
+            paddingTop: 10,
+            paddingBottom: 20
+
+            // backgroundColor: color.white
           }}
-          label='Password'
-          value={password}
-          onChangeText={text => setPassword(text)}
-        />
+        >
+          <CheckBox
+            value={isSelected}
+            tintColors={{ true: color.green, false: color.white }}
+            onValueChange={setSelection}
+            style={styles.checkbox}
+          />
+
+          <TouchableOpacity
+            style={{
+              flex: 1
+            }}
+          >
+            <Text style={[styles.buttonText, styles.forgotPasswordText]}>FORGOT PASSWORD</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
+          <Text style={styles.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
       </View>
-
-      <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
-        <Text style={styles.buttonText}>LOGIN</Text>
-      </TouchableOpacity>
     </View>
   );
 };
@@ -68,8 +111,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
     backgroundColor: color.green
+  },
+
+  button: {
+    backgroundColor: color.white,
+    width: 120,
+    height: 40,
+    borderRadius: 7,
+    top: 15
+  },
+  buttonText: {
+    color: color.primary,
+    paddingTop: 8,
+    flex: 1,
+    borderRadius: 10,
+    textAlign: 'center',
+    fontWeight: 'bold'
   },
 
   // TODO: Rotate background color angle
@@ -94,7 +153,6 @@ const styles = StyleSheet.create({
 
   emailInput: {
     width: '70%',
-    // bottom: 200,
     right: 20,
     bottom: 100,
     justifyContent: 'center',
@@ -103,27 +161,18 @@ const styles = StyleSheet.create({
     color: color.white
   },
 
+  forgotPasswordText: {
+    color: color.white,
+    textDecorationLine: 'underline',
+    textAlign: 'right',
+    right: 10
+  },
+
   logoImage: {
     position: 'absolute',
     top: 100,
     alignSelf: 'center',
     width: 100,
     height: 100
-  },
-
-  button: {
-    backgroundColor: color.white,
-    width: 120,
-    height: 40,
-    borderRadius: 7,
-    bottom: 100
-  },
-  buttonText: {
-    color: color.primary,
-    paddingTop: 8,
-    flex: 1,
-    borderRadius: 10,
-    textAlign: 'center',
-    fontWeight: 'bold'
   }
 });
